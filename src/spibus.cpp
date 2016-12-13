6ceff052cc4932811d808175861a1e83b4a87aa9
Josip Miskovic <jm@spiri.io>
2016-12-13 20:26:01 +0100

    SPI transport layer rewritten as state machine

--- src/spibus.cpp
+++ src/spibus.cpp
@@ -20,7 +20,7 @@ SpiBus::SpiBus(QObject *parent) : QObject(parent)
     }
     fileDescriptor = busDeviceFile.handle();
 
-    const uint8_t mode = 0;
+    const uint8_t mode = 1;
     const uint8_t bits = 8;
     const uint32_t speed = 1000;
     const uint16_t delay = 0;
@@ -86,6 +86,80 @@ void SpiBus::sendMessage(QByteArray payload)
     txArray.append((char)(length & 0xFF));
     txArray.append(payload);
     txArray.append(8, '\x00'); // TODO: CRC
+}
+
+enum parseMachine {
+    STATE_WAITING_PREAMBLE,
+    STATE_WAITING_HEADER,
+    STATE_WAITING_FRAME
+};
+
+void SpiBus::parseRxMessageSM() {
+    static enum parseMachine parsing_state = STATE_WAITING_PREAMBLE;
+    int locPB1 = rxArray.indexOf(PREAMBLE_BYTE1);
+    int locPB2 = rxArray.indexOf(PREAMBLE_BYTE2);
+
+    LOG_DEBUG("parser state: " << parsing_state);
+
+    switch (parsing_state) {
+    case STATE_WAITING_PREAMBLE:
+        if (locPB1 >= 0)
+        {
+            if  (locPB2 == locPB1 + 1)
+            { // full preamble found
+                parsing_state = STATE_WAITING_HEADER;
+                LOG_DEBUG("discarding junk: " << rxArray.mid(0, locPB1).toHex());
+                rxArray = rxArray.mid(locPB1);
+            }
+            else if (locPB1 == rxArray.length() - 1)
+            { // first preamble byte found at end of buffer, but we can discard the rest
+                LOG_DEBUG("discarding junk: " << rxArray.mid(0, locPB1).toHex());
+                rxArray = rxArray.mid(locPB1);
+            } else
+            { // false preamble should be removed
+                LOG_DEBUG("discarding junk: " << rxArray.mid(0, locPB1 + 1).toHex());
+                rxArray = rxArray.mid(locPB1 + 1);
+            }
+        }
+        else if (locPB1 == -1)
+        { // no preamble found and it's safe to discard all
+            LOG_DEBUG("discarding junk: " << rxArray.toHex());
+            rxArray.clear();
+        }
+        else
+        break;
+    case STATE_WAITING_HEADER:
+        if (rxArray.length() >= FRAME_OVERHEAD) {
+            parsing_state = STATE_WAITING_FRAME;
+        }
+        break;
+    case STATE_WAITING_FRAME:
+        if (rxArray.at(2) != MESSAGE_TYPE_PROTOBUF) {
+            LOG_WARN(QString().sprintf("Discarding message with unsupported type 0x%02X", rxArray.at(2)));
+            rxArray = rxArray.mid(2); // remove preamble of unparsable frame
+            parsing_state = STATE_WAITING_PREAMBLE;
+        } else {
+            int frameLength =  (rxArray.at(6) << 8) + rxArray.at(7);
+            if ((frameLength < FRAME_OVERHEAD) || frameLength > MAX_FRAME_LENGTH) {
+                LOG_WARN("Discarding frame of impossible length " << frameLength);
+                rxArray = rxArray.mid(2); // remove preamble of unparsable message
+                parsing_state = STATE_WAITING_PREAMBLE;
+            }
+            else if (frameLength < rxArray.length()) {
+                int messageLength = frameLength - FRAME_OVERHEAD; // removing preamble, type, reserved, length, crc
+                LOG_INFO("SPI complete frame: " << rxArray.mid(0, frameLength).toHex());
+                emit newMessageReceived(rxArray.mid(2 + 1 + 3 + 2, messageLength));
+                rxArray = rxArray.mid(frameLength); // remove frame from buffer
+                parsing_state = STATE_WAITING_PREAMBLE;
+            } else {
+                LOG_INFO("waiting complete frame: " << rxArray.length() << " / " << frameLength);
+            }
+        }
+        break;
+    default:
+        LOG_WARN("Invalid state in parseRxMessageSM");
+        break;
+    }
 }
 
 void SpiBus::parseRxMessage() {
@@ -165,5 +239,5 @@ void SpiBus::pollBus()
     rxArray.append((char *) rx, spiMsgLength);
 
     // try to make sense of stream and remove parsed sections from rxArray
-    parseRxMessage();
+    parseRxMessageSM();
 }
